a6848fa44d61bd8699165a6346d8d4c2f404d0f0
chetrameng <chetrameng422@gmial.com>
2024-02-21 08:28:15 +0700

    first commit

---
+++ src/clas/NumberListProgram.java
@@ -0,0 +1,39 @@
+package clas;
+import java.util.Scanner;
+
+
+public class NumberListProgram {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter numbers (enter 'done' to finish):");
+
+        double[] numbers = new double[1]; // Start with an array of size 1
+        int count = 0;
+
+        while (true) {
+            String input = scanner.next();
+            if (input.equalsIgnoreCase("done")) {
+                break;
+            }
+            try {
+                double number = Double.parseDouble(input);
+                if (count == numbers.length) {
+                    // Resize the array when it's full
+                    double[] temp = new double[numbers.length * 2];
+                    System.arraycopy(numbers, 0, temp, 0, numbers.length);
+                    numbers = temp;
+                }
+                numbers[count] = number;
+                count++;
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input. Please enter a number or 'done' to finish.");
+            }
+        }
+
+        System.out.println("Input numbers are:");
+        for (int i = 0; i < count; i++) {
+            System.out.println(numbers[i]);
+        }
+    }
+}
